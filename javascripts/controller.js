440f5f85cf22f2e8fabb7fd779d85bec6b64ab97
Rosano Cangussu Galvão Alves <rosanocangussu@gmail.com>
2013-03-20 13:08:22 -0300

    Create controller.js

---
+++ javascripts/controller.js
@@ -0,0 +1,6 @@
+function ListaComprasController($scope) {
+    $scope.itens = [
+        {produto: 'Leite', quantidade: 2, comprado: false},
+        {produto: 'Cerveja', quantidade: 12, comprado: false}
+    ];
+}
