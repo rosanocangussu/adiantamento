03473a72cb6a7c0c0e6e16f432417aea9979ca35
Rosano Cangussu Galvão Alves <rosanocangussu@gmail.com>
2013-03-20 14:30:00 -0300

    Update controller.js

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -1,11 +1,12 @@
 function ListaComprasController($scope) {
     $scope.itens = [
-        {produto: 'Leite', quantidade: 2, comprado: false},
-        {produto: 'Cerveja', quantidade: 12, comprado: false}
+        {id: 1, produto: 'Leite', quantidade: 2, comprado: false},
+        {id: 2, produto: 'Cerveja', quantidade: 12, comprado: false}
     ];
     
     $scope.adicionaItem = function () {
-        $scope.itens.push({produto: $scope.item.produto,
+        var quant = $scope.itens.length();
+        $scope.itens.push({id: quant++ ,produto: $scope.item.produto,
                            quantidade: $scope.item.quantidade,
                            comprado: false});
         $scope.item.produto = $scope.item.quantidade = '';
