e5531888f242abde2d12a2e6352b4ab480fdbf45
Rosano Cangussu Galvão Alves <rosanocangussu@gmail.com>
2013-03-21 17:02:03 -0300

    Update controller.js

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -14,7 +14,7 @@ function ListaComprasController($scope) {
     
      $scope.deletaItem = function () {
       var botao = document.getElementById("botao").value;
-      
+      console.log(botao);
       var n = botao.toString();
        document.getElementById(n).style.display = 'none';
     };
